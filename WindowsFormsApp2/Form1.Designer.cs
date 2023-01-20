9c7719c8249c8d2b02dfba03b3bf9d5e0f4a0202
felipesan88 <121824760+felipesan88@users.noreply.github.com>
2023-01-20 11:12:17 -0300

    calculadora

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class LGTV
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao1 = new System.Windows.Forms.Button();
+            this.botao2 = new System.Windows.Forms.Button();
+            this.botao3 = new System.Windows.Forms.Button();
+            this.botaosoma = new System.Windows.Forms.Button();
+            this.botaoce = new System.Windows.Forms.Button();
+            this.botao5 = new System.Windows.Forms.Button();
+            this.botao8 = new System.Windows.Forms.Button();
+            this.botao6 = new System.Windows.Forms.Button();
+            this.botaomenos = new System.Windows.Forms.Button();
+            this.botao9 = new System.Windows.Forms.Button();
+            this.botaox = new System.Windows.Forms.Button();
+            this.botao4 = new System.Windows.Forms.Button();
+            this.botao7 = new System.Windows.Forms.Button();
+            this.botao0 = new System.Windows.Forms.Button();
+            this.botaodivisao = new System.Windows.Forms.Button();
+            this.labelOperacao = new System.Windows.Forms.Label();
+            this.botaoresultado = new System.Windows.Forms.Button();
+            this.botaoc = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // botao1
+            // 
+            this.botao1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao1.Location = new System.Drawing.Point(8, 49);
+            this.botao1.Name = "botao1";
+            this.botao1.Size = new System.Drawing.Size(50, 80);
+            this.botao1.TabIndex = 0;
+            this.botao1.Text = "1";
+            this.botao1.UseVisualStyleBackColor = false;
+            this.botao1.Click += new System.EventHandler(this.botao1_Click);
+            // 
+            // botao2
+            // 
+            this.botao2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao2.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao2.Location = new System.Drawing.Point(69, 49);
+            this.botao2.Name = "botao2";
+            this.botao2.Size = new System.Drawing.Size(50, 80);
+            this.botao2.TabIndex = 1;
+            this.botao2.Text = "2";
+            this.botao2.UseVisualStyleBackColor = false;
+            this.botao2.Click += new System.EventHandler(this.botao2_Click);
+            // 
+            // botao3
+            // 
+            this.botao3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao3.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao3.Location = new System.Drawing.Point(125, 49);
+            this.botao3.Name = "botao3";
+            this.botao3.Size = new System.Drawing.Size(50, 80);
+            this.botao3.TabIndex = 2;
+            this.botao3.Text = "3";
+            this.botao3.UseVisualStyleBackColor = false;
+            this.botao3.Click += new System.EventHandler(this.botao3_Click);
+            // 
+            // botaosoma
+            // 
+            this.botaosoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaosoma.ForeColor = System.Drawing.Color.MediumSeaGreen;
+            this.botaosoma.Location = new System.Drawing.Point(181, 49);
+            this.botaosoma.Name = "botaosoma";
+            this.botaosoma.Size = new System.Drawing.Size(50, 80);
+            this.botaosoma.TabIndex = 3;
+            this.botaosoma.Text = "+";
+            this.botaosoma.UseVisualStyleBackColor = true;
+            this.botaosoma.Click += new System.EventHandler(this.botaosoma_Click);
+            // 
+            // botaoce
+            // 
+            this.botaoce.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.asdsa;
+            this.botaoce.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoce.Location = new System.Drawing.Point(237, 49);
+            this.botaoce.Name = "botaoce";
+            this.botaoce.Size = new System.Drawing.Size(50, 80);
+            this.botaoce.TabIndex = 4;
+            this.botaoce.Text = "CE";
+            this.botaoce.UseVisualStyleBackColor = true;
+            this.botaoce.Click += new System.EventHandler(this.botaoce_Click);
+            // 
+            // botao5
+            // 
+            this.botao5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao5.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao5.Location = new System.Drawing.Point(69, 135);
+            this.botao5.Name = "botao5";
+            this.botao5.Size = new System.Drawing.Size(50, 80);
+            this.botao5.TabIndex = 5;
+            this.botao5.Text = "5";
+            this.botao5.UseVisualStyleBackColor = false;
+            this.botao5.Click += new System.EventHandler(this.botao5_Click);
+            // 
+            // botao8
+            // 
+            this.botao8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao8.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao8.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao8.Location = new System.Drawing.Point(69, 221);
+            this.botao8.Name = "botao8";
+            this.botao8.Size = new System.Drawing.Size(50, 80);
+            this.botao8.TabIndex = 6;
+            this.botao8.Text = "8";
+            this.botao8.UseVisualStyleBackColor = false;
+            this.botao8.Click += new System.EventHandler(this.botao8_Click);
+            // 
+            // botao6
+            // 
+            this.botao6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao6.ForeColor = System.Drawing.Color.Black;
+            this.botao6.Location = new System.Drawing.Point(125, 135);
+            this.botao6.Name = "botao6";
+            this.botao6.Size = new System.Drawing.Size(50, 80);
+            this.botao6.TabIndex = 7;
+            this.botao6.Text = "6";
+            this.botao6.UseVisualStyleBackColor = false;
+            this.botao6.Click += new System.EventHandler(this.botao6_Click);
+            // 
+            // botaomenos
+            // 
+            this.botaomenos.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaomenos.ForeColor = System.Drawing.Color.Red;
+            this.botaomenos.Location = new System.Drawing.Point(181, 135);
+            this.botaomenos.Name = "botaomenos";
+            this.botaomenos.Size = new System.Drawing.Size(50, 80);
+            this.botaomenos.TabIndex = 8;
+            this.botaomenos.Text = "-";
+            this.botaomenos.UseVisualStyleBackColor = true;
+            this.botaomenos.Click += new System.EventHandler(this.botaomenos_Click);
+            // 
+            // botao9
+            // 
+            this.botao9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao9.ForeColor = System.Drawing.Color.Black;
+            this.botao9.Location = new System.Drawing.Point(125, 221);
+            this.botao9.Name = "botao9";
+            this.botao9.Size = new System.Drawing.Size(50, 80);
+            this.botao9.TabIndex = 10;
+            this.botao9.Text = "9";
+            this.botao9.UseVisualStyleBackColor = false;
+            this.botao9.Click += new System.EventHandler(this.botao9_Click);
+            // 
+            // botaox
+            // 
+            this.botaox.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaox.ForeColor = System.Drawing.Color.Yellow;
+            this.botaox.Location = new System.Drawing.Point(181, 221);
+            this.botaox.Name = "botaox";
+            this.botaox.Size = new System.Drawing.Size(50, 80);
+            this.botaox.TabIndex = 11;
+            this.botaox.Text = "X";
+            this.botaox.UseVisualStyleBackColor = true;
+            this.botaox.Click += new System.EventHandler(this.botaox_Click);
+            // 
+            // botao4
+            // 
+            this.botao4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao4.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.botao4.Location = new System.Drawing.Point(8, 135);
+            this.botao4.Name = "botao4";
+            this.botao4.Size = new System.Drawing.Size(50, 80);
+            this.botao4.TabIndex = 13;
+            this.botao4.Text = "4";
+            this.botao4.UseVisualStyleBackColor = false;
+            this.botao4.Click += new System.EventHandler(this.botao4_Click);
+            // 
+            // botao7
+            // 
+            this.botao7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.botao7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.botao7.Location = new System.Drawing.Point(8, 221);
+            this.botao7.Name = "botao7";
+            this.botao7.Size = new System.Drawing.Size(50, 80);
+            this.botao7.TabIndex = 14;
+            this.botao7.Text = "7";
+            this.botao7.UseVisualStyleBackColor = false;
+            this.botao7.Click += new System.EventHandler(this.botao7_Click);
+            // 
+            // botao0
+            // 
+            this.botao0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botao0.ForeColor = System.Drawing.Color.Crimson;
+            this.botao0.Location = new System.Drawing.Point(8, 306);
+            this.botao0.Name = "botao0";
+            this.botao0.Size = new System.Drawing.Size(167, 38);
+            this.botao0.TabIndex = 15;
+            this.botao0.Text = "0";
+            this.botao0.UseVisualStyleBackColor = true;
+            this.botao0.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // botaodivisao
+            // 
+            this.botaodivisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaodivisao.ForeColor = System.Drawing.Color.Lime;
+            this.botaodivisao.Location = new System.Drawing.Point(181, 306);
+            this.botaodivisao.Name = "botaodivisao";
+            this.botaodivisao.Size = new System.Drawing.Size(50, 38);
+            this.botaodivisao.TabIndex = 16;
+            this.botaodivisao.Text = "/";
+            this.botaodivisao.UseVisualStyleBackColor = true;
+            this.botaodivisao.Click += new System.EventHandler(this.botaodivisao_Click);
+            // 
+            // labelOperacao
+            // 
+            this.labelOperacao.AutoSize = true;
+            this.labelOperacao.BackColor = System.Drawing.Color.Turquoise;
+            this.labelOperacao.Location = new System.Drawing.Point(5, 9);
+            this.labelOperacao.Name = "labelOperacao";
+            this.labelOperacao.Size = new System.Drawing.Size(52, 13);
+            this.labelOperacao.TabIndex = 17;
+            this.labelOperacao.Text = "operacao";
+            this.labelOperacao.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // botaoresultado
+            // 
+            this.botaoresultado.BackColor = System.Drawing.Color.LightCoral;
+            this.botaoresultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoresultado.Location = new System.Drawing.Point(237, 224);
+            this.botaoresultado.Name = "botaoresultado";
+            this.botaoresultado.Size = new System.Drawing.Size(50, 124);
+            this.botaoresultado.TabIndex = 12;
+            this.botaoresultado.Text = "=";
+            this.botaoresultado.UseVisualStyleBackColor = false;
+            this.botaoresultado.Click += new System.EventHandler(this.botaoresultado_Click);
+            // 
+            // botaoc
+            // 
+            this.botaoc.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.asdsa;
+            this.botaoc.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.botaoc.Location = new System.Drawing.Point(237, 135);
+            this.botaoc.Name = "botaoc";
+            this.botaoc.Size = new System.Drawing.Size(50, 80);
+            this.botaoc.TabIndex = 9;
+            this.botaoc.Text = "C";
+            this.botaoc.UseVisualStyleBackColor = true;
+            this.botaoc.Click += new System.EventHandler(this.botaoc_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(6, 25);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(279, 20);
+            this.resultado.TabIndex = 18;
+            // 
+            // LGTV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.BackgroundImage = global::WindowsFormsApp2.Properties.Resources.aaa;
+            this.ClientSize = new System.Drawing.Size(297, 360);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.labelOperacao);
+            this.Controls.Add(this.botaodivisao);
+            this.Controls.Add(this.botao0);
+            this.Controls.Add(this.botao7);
+            this.Controls.Add(this.botao4);
+            this.Controls.Add(this.botaoresultado);
+            this.Controls.Add(this.botaox);
+            this.Controls.Add(this.botao9);
+            this.Controls.Add(this.botaoc);
+            this.Controls.Add(this.botaomenos);
+            this.Controls.Add(this.botao6);
+            this.Controls.Add(this.botao8);
+            this.Controls.Add(this.botao5);
+            this.Controls.Add(this.botaoce);
+            this.Controls.Add(this.botaosoma);
+            this.Controls.Add(this.botao3);
+            this.Controls.Add(this.botao2);
+            this.Controls.Add(this.botao1);
+            this.Name = "LGTV";
+            this.Text = "LGBT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao1;
+        private System.Windows.Forms.Button botao2;
+        private System.Windows.Forms.Button botao3;
+        private System.Windows.Forms.Button botaosoma;
+        private System.Windows.Forms.Button botaoce;
+        private System.Windows.Forms.Button botao5;
+        private System.Windows.Forms.Button botao8;
+        private System.Windows.Forms.Button botao6;
+        private System.Windows.Forms.Button botaomenos;
+        private System.Windows.Forms.Button botaoc;
+        private System.Windows.Forms.Button botao9;
+        private System.Windows.Forms.Button botaox;
+        private System.Windows.Forms.Button botaoresultado;
+        private System.Windows.Forms.Button botao4;
+        private System.Windows.Forms.Button botao7;
+        private System.Windows.Forms.Button botao0;
+        private System.Windows.Forms.Button botaodivisao;
+        private System.Windows.Forms.Label labelOperacao;
+        private System.Windows.Forms.TextBox resultado;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class LGTV : Form
+    {
+        decimal valor1 = 0;
+        decimal valor2 = 0;
+        string operacao = "";
+
+
+
+
+        public LGTV()
+        {
+            InitializeComponent();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "0";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void botaosoma_Click(object sender, EventArgs e)
+        {
+           valor1 = decimal.Parse(resultado.Text,CultureInfo.InvariantCulture);
+            resultado.Text = "";
+            operacao = "soma";
+            labelOperacao.Text = "+";
+            valor1 = 0;
+            valor2 = 0;
+        }
+
+        private void botao9_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "9";
+        }
+
+        private void botao7_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "7";
+
+
+
+        }
+
+        private void botao2_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "2";
+        }
+
+        private void botao3_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "3";
+        }
+
+        private void botao4_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "4";
+        }
+
+        private void botao5_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "5";
+        }
+
+        private void botao6_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "6";
+        }
+
+        private void botao8_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "8";
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            resultado.Text = resultado.Text + "1";
+        }
+
+        private void botaomenos_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(resultado.Text,CultureInfo.InvariantCulture);
+            resultado.Text = "";
+            operacao = "menos";
+            labelOperacao.Text = "-";
+        }
+
+        private void botaoce_Click(object sender, EventArgs e)
+        {
+            resultado.Text = "";
+        }
+
+        private void botaoc_Click(object sender, EventArgs e)
+        {
+            resultado.Text = "";
+            labelOperacao.Text = "";
+            valor2 = 0;
+            valor1 = 0;
+
+        }
+
+        private void botaox_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(resultado.Text, CultureInfo.InvariantCulture);
+            resultado.Text = "";
+            operacao = "mult";
+            labelOperacao.Text = "X";
+        }
+
+        private void botaodivisao_Click(object sender, EventArgs e)
+        {
+            valor1 = decimal.Parse(resultado.Text, CultureInfo.InvariantCulture);
+            resultado.Text = "";
+            operacao = "div";
+            labelOperacao.Text = "/";
+            
+
+        
+        
+        }
+
+        private void botaoresultado_Click(object sender, EventArgs e)
+        {
+            valor2 = decimal.Parse(resultado.Text, CultureInfo.InvariantCulture);
+            if (operacao == "soma")
+            {
+                resultado.Text = Convert.ToString(valor1 + valor2);
+            }
+
+            if (operacao == "menos")
+            {
+                resultado.Text = Convert.ToString(valor1 - valor2);
+            }
+            if (operacao == "div")
+            {
+                resultado.Text = Convert.ToString(valor1 / valor2);
+            }
+            if (operacao == "mult")
+            {
+                resultado.Text = Convert.ToString(valor1 * valor2);
+            }
+
+
+        }
+    }
+}
